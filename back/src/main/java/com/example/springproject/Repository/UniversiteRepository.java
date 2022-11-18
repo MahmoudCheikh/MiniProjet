3519ddfb167698bfa28749117c01010d51cd9c2d
Nacib Mariem <mariem.nacib@esprit.tn>
2022-11-18 13:23:12 +0100

    CRUD UNIVERSITE

--- back/src/main/java/com/example/springproject/Repository/UniversiteRepository.java
+++ back/src/main/java/com/example/springproject/Repository/UniversiteRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UniversiteRepository extends CrudRepository<Universite,Long> {
+public interface UniversiteRepository extends CrudRepository<Universite,Integer> {
 }
--- back/src/main/java/com/example/springproject/Service/IUniversiteService.java
+++ back/src/main/java/com/example/springproject/Service/IUniversiteService.java
@@ -1,4 +1,17 @@
 package com.example.springproject.Service;
 
+import com.example.springproject.Entity.Universite;
+
+import java.util.List;
+
 public interface IUniversiteService {
+    int ajouterUniversite(Universite C);
+
+    Boolean deleteUniversite(Universite C);
+
+    Boolean updateUniversite(Universite C);
+
+    List<Universite> findAll();
+
+    Universite findById(Integer C);
 }
