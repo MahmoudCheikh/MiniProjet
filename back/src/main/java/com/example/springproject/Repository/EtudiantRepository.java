0c6eddb100419c36bdfc967df40767f1fa2b7737
Nacib Mariem <mariem.nacib@esprit.tn>
2022-11-18 13:29:20 +0100

    CRUD ETUDIANT

--- back/src/main/java/com/example/springproject/Repository/EtudiantRepository.java
+++ back/src/main/java/com/example/springproject/Repository/EtudiantRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EtudiantRepository extends CrudRepository<Etudiant,Long> {
+public interface EtudiantRepository extends CrudRepository<Etudiant,Integer> {
 }
