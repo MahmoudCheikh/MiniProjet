cc1970d13ac29faee1f335f05b691a249cf742f5
Nacib Mariem <mariem.nacib@esprit.tn>
2022-11-29 08:19:42 +0100

    CRUD COURS

---
+++ back/src/main/java/com/example/springproject/Entity/Cours.java
@@ -0,0 +1,28 @@
+package com.example.springproject.Entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Cours")
+public class Cours {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_cours")
+    private Integer IdCours;
+    private Module Nom;
+    private String description;
+    private Integer credit;
+    private Integer nbHeure;
+    private Integer anneeUniv;
+
+}
---
+++ back/src/main/java/com/example/springproject/Entity/Module.java
@@ -0,0 +1,5 @@
+package com.example.springproject.Entity;
+
+public enum Module {
+    MB1,MB2,MB3,MB4,TE,UNIX,SPRING_BOOT,ANGULAR;
+}
